86f83f2d7e6a3135b7e2806aebc2826d11616a83
RawadHaidar <rawadahaidar@gmail.com>
2025-05-31 16:38:38 +0300

    reformat printed results

--- assignment2.py
+++ assignment2.py
@@ -1,17 +1,17 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 gpa = float(input("Enter your GPA: "))
-graduated = input("Have you graduated? (yes/no): \n")
+graduated = input("Have you graduated? (yes/no): ")
 
-print("Hello,", name,"!")
+print(f"\nHello, {name}!")
 print("Age:", age)
 print("GPA:", gpa)
 print("Graduated:", graduated, "\n")
 print("Analyzing your data...\n")
 
 if age < 25 and gpa >= 3.5 and graduated.lower() == "yes":
-    print("You are eligible for the scholarship!\n")
+    print("result: You are eligible for the scholarship!\n")
 elif age < 30 and gpa >= 2.5 and graduated.lower() == "no":
-    print("You are eligible for the internship program!\n")
+    print("result: You are eligible for the internship program!\n")
 else:
-    print("You are not eligible, please try again later.\n")
+    print("result: You are not eligible, please try again later.\n")
